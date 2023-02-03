f4cd2a8a3c9b2890209e54114f225183496102f3
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2023-02-03 13:13:39 +0100

    Añado Vehiculo.java

---
+++ Vehiculo.java
@@ -0,0 +1,16 @@
+
+public class Vehiculo {
+  
+  String marca;
+  String modelo;
+  String matricula;
+  
+  
+  public Vehiculo(String marca, String modelo, String matricula) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.matricula = matricula;
+  }
+  
+  
+}
